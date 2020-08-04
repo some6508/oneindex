6105e1b1dee04b584c59056fcfdc7fa73133ad7a
董小豪 <58564994+some6508@users.noreply.github.com>
2020-08-04 09:52:48 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,106 @@
+<?php return array (
+  'site_name' => '某人の网盘',
+  'password' => '3180965797',
+  'style' => 'nexmoe',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'h27zG8pr8BNsLU0JbBh5AOznNS5Of5Y540l/koc7048=',
+  'client_id' => 'ea2b36f6-b8ad-40be-bc0f-e5e4a4a7d4fa',
+  'redirect_uri' => 'https://moeclub.org/onedrive-login',
+  'refresh_token' => '0.AS4AFX-7p8LmEkSjOtYrGsTOfPmUIOZdww1KlzHpFBQxcWkuAC0.AQABAAAAAABeAFzDwllzTYGDLh_qYbH8XKIZpdL9cUQGOLQZ5JtoQHgMw1W6IVgjM65ab4bW9i7tBvJiMohl6rrd-toepNGjFtHaRq1cQmV1JFY7ZGg70IlUmntZ6P0wBIP0K5Up9RhvpsMFqmLCoSHSoAJ_sLuoe5XSWeF0Z7NdhCAfWaMDl4C70dUybNEHrUuQLy2EcW6c3l5a3X_X7zqbE-jh1GI8tR9gNoNfaeve0exJJHIYE_UuwDrCOSqnvc-XNhJ99nLJDrosYs3QxuLdpebrDRVmIXltxPJNSHnO6RSuwBuPnZadKegc_Vo6JKHfYheK9MAa2s8_ObcI_ksaXqQY-zWaZVLrzs9FeUCC4xaQzhJrBSKmrv9m0J2TSqiUOVY7m-cxTlKtiRYZ203QVV9YkxReo2jqjx9ybZISNjNp2xyU8y5JEhcmUppVXeTDUEuEg2C5f0cFGWYaQlu43CHK8_WbCaCQjms-a_w9N2H9Hgdkz5vSK18sneCq3xwKh2VeFe3ivanLU6Lc9QohcyaRNzUtu-NCN1jObdF9_4Ac0YIEhD40GN-v70m0IJj7hFv9PEbEnKXAW-fzHvdHt5sFPMt7jg67SoRDpnbFDCiRRbpQA3CuAWXaai3Fd7BOWPuv_Zng97Rc8ZDYS7KBC3hk7R2CMH6G-4UXirSDoYew5Jr_CFDo5RtiDTQEqDC1IEmxbqLhSEeoyeqwUuqnELLcweLJDOxaeIhlsy16QRDf_3icGyAA',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
